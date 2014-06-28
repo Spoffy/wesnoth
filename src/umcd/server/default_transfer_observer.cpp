a94789d089df7c08a161cdaee0e852a83e156537
Spoffy <Spoffeh@gmail.com>
2014-06-28 01:44:20 +0100

    Removed unnecessary global namespace ::

--- src/umcd/server/default_transfer_observer.cpp
+++ src/umcd/server/default_transfer_observer.cpp
@@ -16,7 +16,7 @@
 #include "default_transfer_observer.hpp"
 #include "umcd/logger/asio_logger.hpp"
 
-void umcd::default_transfer_observer::transfer_error( const ::boost::system::error_code& error ) {
+void umcd::default_transfer_observer::transfer_error( const boost::system::error_code& error ) {
   UMCD_LOG(error) << "Transfer error: " << error.message();
 }
   
